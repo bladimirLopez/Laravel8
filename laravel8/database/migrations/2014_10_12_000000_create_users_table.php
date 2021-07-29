ed9428da947c92f7e530f63e1044801be370287e
bladimir balderrama lopez <bladimirbalderramalopez123@gmail.com>
2021-07-29 01:36:19 -1100

    migrando las tablas por defecto de laravel 8

--- laravel8/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel8/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->id();//Integer Unsigned Increment
+            $table->string('name');//varchar 255 character
+            $table->string('email')->unique();//unique para proteger y es unico.
+            $table->timestamp('email_verified_at')->nullable();//para guardar fecha
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->rememberToken();//varchar se almacena 1 token
+            $table->timestamps();//crea 2 columnas de created y updated
         });
     }
 
